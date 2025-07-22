0836d5336c4532fd856fac7520a4b9c6dabfb5cb
marleypm16 <yarlempm16@gmail.com>
2025-07-22 07:28:23 -0300

    Janela para buscar palavras

---
+++ NotePad/FindDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace NotePad
+{
+    public partial class FindDialog : Window
+    {
+        private MainWindow parentWindow;
+
+        public FindDialog(MainWindow parent)
+        {
+            InitializeComponent();
+            parentWindow = parent;
+            Owner = parent;
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = searchTextBox.Text;
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                bool matchCase = matchCaseCheckBox.IsChecked ?? false;
+                parentWindow.FindText(searchText, matchCase);
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+        }
+
+        protected override void OnActivated(System.EventArgs e)
+        {
+            base.OnActivated(e);
+            searchTextBox.Focus();
+            searchTextBox.SelectAll();
+        }
+    }
+}
